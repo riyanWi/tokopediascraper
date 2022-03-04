ad46412ac1c944ea6270f89b72c5aaeb7073b728
riyanWi <tunggalrahayu1@gmail.com>
2022-03-05 00:22:08 +0700

    fixing seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,6 +1,7 @@
 pages <<{
     url: "https://www.tokopedia.com/search?navsource=home&q=red%20bull&st=product",
     fetch_type: "browser",
+    page_type: "listings",
     method: "GET",
     headers: {
         "User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
