d6c33e9053314a06c2d67c39064fd144ab2f755e
riyanWi <tunggalrahayu1@gmail.com>
2022-03-05 00:16:44 +0700

    add config and seeder

---
+++ seeder/seeder.rb
@@ -0,0 +1,8 @@
+pages <<{
+    url: "https://www.tokopedia.com/search?navsource=home&q=red%20bull&st=product",
+    fetch_type: "browser",
+    method: "GET",
+    headers: {
+        "User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+    }
+}
